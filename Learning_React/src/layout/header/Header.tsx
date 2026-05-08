1d9156a8e97ad675ba7d86aacb512977c184da16
Anh Tuan <thananhtuan2011@gmail.com>
2026-05-08 21:42:02 +0700

    init FE

---
+++ Learning_React/src/layout/header/Header.tsx
@@ -0,0 +1,19 @@
+import { Button } from 'antd'
+import React, { useEffect } from 'react'
+
+export default function Header() {
+    useEffect(() => {
+        console.log('header')
+    }, [])
+    return (
+        <>
+            <div className='font-bold text-2xl text-red-500'>header </div>
+            <div>
+                <Button type="link" htmlType="submit">
+                    Hello Ant Design
+                </Button>
+                sss
+            </div>
+        </>
+    )
+}
